69234b879d3f58a67de5eaa174ce6d3a0a5f7844
rikiparata <rikiipee47@gmail.com>
2018-06-29 11:48:34 +1200

    pre-heroku

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,8 +5,14 @@ import Form from './Form'
 const Home = (props) => {
     return (
         <div>
-            <h1>Pokemon EDA</h1>
-            <Form updateState={props.updateState} />
+            <div className="hero is-primary is-fullheight has-text-centered">
+                <div className="hero-head">
+                    <h1 className="title">Pokemon of EDA</h1>
+                </div>
+                <div className="hero-body has-content-centered">
+                    <Form updateState={props.updateState} />
+                </div>
+            </div>
         </div>
     )
 }
